72b0732a6023065e52f457c9df4419fa3d700c6c
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-02-20 22:22:03 +0300

    Add: new routes and queryProvider

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "../src/pages/Login";
 import Dashboard from "../src/pages/Dashboard";
 import PrivateRoute from "../src/routes/PrivateRoute";
+import SignUp from "./pages/SignUp";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route
           path="/dashboard"
           element={
